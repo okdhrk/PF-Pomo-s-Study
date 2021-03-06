ec6317db1a9e13dbf1f00afb4cbbaf8400bbbb11
EC2 Default User <ec2-user@ip-172-31-42-237.ec2.internal>
2021-03-06 09:22:39 +0000

    Userコントローラー、ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
-  get 'tweets/index'
-  get 'tweets/show'
-  get 'tweets/new'
-  get 'tweets/destroy'
   root to: 'homes#top'
+
+  resources :users, only: [:edit, :index, :show, :update]
+  get 'users/unsubscribe' => 'users#unsubscribe'
+
+
+  resources :tweets, only: [:destroy, :index, :new, :show]
+
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
